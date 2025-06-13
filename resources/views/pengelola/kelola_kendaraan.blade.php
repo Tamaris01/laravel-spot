613fa73cb1a6a3ea57bdf7e7dfe5ed1d989ece0c
Tamaris Silitonga <tamarissilitonga@gmail.com>
2025-06-13 15:17:32 +0700

    Fix

--- resources/views/pengelola/kelola_kendaraan.blade.php
+++ resources/views/pengelola/kelola_kendaraan.blade.php
@@ -370,20 +370,12 @@
                                 </div>
 
                                 <!-- Modal Edit Kendaraan -->
-                                @foreach ($dataKendaraan as $data)
-                                <td>
-                                    <button class="btn btn-info btn-sm" data-toggle="modal" data-target="#editModal{{ $data->plat_nomor }}">
-                                        <i class="fas fa-edit"></i> Edit
-                                    </button>
-                                </td>
-
-                                <!-- Modal Edit Kendaraan -->
-                                <div class="modal fade" id="editModal{{ $data->plat_nomor }}" tabindex="-1" role="dialog" aria-labelledby="editModalLabel{{ $data->plat_nomor }}" aria-hidden="true">
+                                <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-centered modal-lg">
                                         <div class="modal-content">
                                             <!-- Modal Header -->
                                             <div class="modal-header" style="background-color: #FFDC40;">
-                                                <h5 class="modal-title" id="editModalLabel{{ $data->plat_nomor }}">Edit Kendaraan</h5>
+                                                <h5 class="modal-title" id="editModalLabel">Edit Kendaraan</h5>
                                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
@@ -391,20 +383,23 @@
 
                                             <!-- Modal Body -->
                                             <div class="modal-body">
-                                                <form method="POST" enctype="multipart/form-data"
+                                                <!-- Begin Form -->
+                                                <form method="POST" id="editForm" enctype="multipart/form-data"
                                                     action="{{ route('pengelola.kelola_kendaraan.update', ['plat_nomor' => $data->plat_nomor]) }}">
                                                     @csrf
                                                     @method('PUT')
 
                                                     <div class="row">
-                                                        <!-- Left Column: Upload Foto -->
+                                                        <!-- Left Column: Upload Foto Kendaraan -->
                                                         <div class="col-md-6 text-center" style="border-right: 1px solid black;">
-                                                            <div class="upload-area">
+                                                            <div class="upload-area" onclick="document.getElementById('uploadPhotoVehicleEdit').click()">
                                                                 <img
-                                                                    src="{{ $data->foto }}"
-                                                                    alt="Foto Kendaraan"
+                                                                    id="previewVehicleEdit"
+                                                                    src="" {{-- Nanti diisi lewat JavaScript secara dinamis --}}
+                                                                    alt="Preview Foto Kendaraan"
                                                                     style="display:block; max-width:80%; border-radius: 5px;"
                                                                     class="img-fluid mb-3" />
+
 
                                                                 <div style="position: absolute; bottom: 0; left: 0; right: 0; background-color: #FFDC40; color: black; padding: 10px; border-top: 1px solid black; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; cursor: pointer;">
                                                                     <i class="fas fa-plus-circle fa-2x"></i>
@@ -412,26 +407,32 @@
 
                                                                 <input
                                                                     type="file"
+                                                                    id="uploadPhotoVehicleEdit"
                                                                     name="foto_kendaraan"
                                                                     style="display: none;"
-                                                                    accept="image/*">
+                                                                    accept="image/*"
+                                                                    onchange="previewImage(event, 'previewVehicleEdit')">
                                                             </div>
                                                             @error('foto_kendaraan')
                                                             <div class="text-danger">{{ $message }}</div>
                                                             @enderror
                                                         </div>
 
-                                                        <!-- Right Column -->
+
+                                                        <!-- Right Column: Input Fields -->
                                                         <div class="col-md-6">
-                                                            <!-- ID Pengguna -->
+                                                            <!-- ID Pengguna (Opsional) -->
                                                             <div class="form-group">
                                                                 <div class="input-group">
                                                                     <span class="input-group-text"><i class="fas fa-id-card"></i></span>
-                                                                    <select name="id_pengguna" class="form-control">
-                                                                        <option value="" disabled>Pilih ID Pengguna</option>
+                                                                    <select id="id_pengguna_edit" name="id_pengguna" class="form-control">
+                                                                        <option value="" disabled selected>Pilih ID Pengguna</option> <!-- Placeholder option -->
                                                                         @foreach($penggunaDropdown as $pengguna)
                                                                         <option value="{{ $pengguna->id_pengguna }}"
-                                                                            {{ optional($data->penggunaParkir)->id_pengguna == $pengguna->id_pengguna ? 'selected' : '' }}>
+                                                                            @if($pengguna->id_pengguna == old('id_pengguna', $data->id_pengguna))
+                                                                            selected
+                                                                            @endif
+                                                                            >
                                                                             {{ $pengguna->id_pengguna }} - {{ $pengguna->nama }}
                                                                         </option>
                                                                         @endforeach
@@ -439,22 +440,22 @@
                                                                 </div>
                                                             </div>
 
-                                                            <!-- Plat Nomor -->
                                                             <div class="form-group">
                                                                 <div class="input-group">
                                                                     <span class="input-group-text"><i class="fas fa-car"></i></span>
-                                                                    <input type="text" name="plat_nomor_lama"
+                                                                    <!-- Input field for plat_nomor (disabled to prevent editing) -->
+                                                                    <input type="text" name="plat_nomor_lama" id="plat_nomor_edit"
                                                                         class="form-control"
-                                                                        value="{{ $data->plat_nomor }}"
+                                                                        value="{{ old('plat_nomor', $data->plat_nomor) }}"
                                                                         disabled>
                                                                 </div>
                                                             </div>
 
-                                                            <!-- Jenis -->
+                                                            <!-- Jenis Kendaraan -->
                                                             <div class="form-group">
                                                                 <div class="input-group">
                                                                     <span class="input-group-text"><i class="fas fa-car"></i></span>
-                                                                    <select name="jenis" class="form-control" required>
+                                                                    <select id="jenis_edit" name="jenis" class="form-control" required>
                                                                         @foreach($jenisArray as $jenis)
                                                                         <option value="{{ $jenis }}" {{ $data->jenis == $jenis ? 'selected' : '' }}>
                                                                             {{ ucfirst($jenis) }}
@@ -464,11 +465,11 @@
                                                                 </div>
                                                             </div>
 
-                                                            <!-- Warna -->
+                                                            <!-- Warna Kendaraan -->
                                                             <div class="form-group">
                                                                 <div class="input-group">
                                                                     <span class="input-group-text"><i class="fas fa-paint-brush"></i></span>
-                                                                    <select name="warna" class="form-control" required>
+                                                                    <select id="warna_edit" name="warna" class="form-control" required>
                                                                         @foreach($warnaArray as $warna)
                                                                         <option value="{{ $warna }}" {{ $data->warna == $warna ? 'selected' : '' }}>
                                                                             {{ ucfirst($warna) }}
@@ -478,19 +479,23 @@
                                                                 </div>
                                                             </div>
                                                         </div>
+
                                                     </div>
 
-                                                    <!-- Footer -->
+                                                    <!-- Modal Footer -->
                                                     <div class="modal-footer" style="border-top: 1px solid black;">
+                                                        <!-- Cancel Button -->
                                                         <button type="button" class="btn" style="background-color: #fff; color: #000; margin-right: 8px; border: 1px solid #000; width: 100px;" data-dismiss="modal">Batal</button>
+
+                                                        <!-- Save Button -->
                                                         <button type="submit" class="btn" style="background-color: #FFDC40; color: black; width: 100px">Simpan</button>
                                                     </div>
                                                 </form>
+                                                <!-- End Form -->
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
 
                                 @endforeach
                             </tbody>
