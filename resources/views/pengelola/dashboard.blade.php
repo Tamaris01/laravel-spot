ca0d535aa7e4e5d01162bd74a7d5835e9b84ef94
Tamaris01 <tamarissilitonga@gmail.com>
2025-07-15 15:09:40 +0700

    Fix

--- resources/views/pengelola/dashboard.blade.php
+++ resources/views/pengelola/dashboard.blade.php
@@ -153,7 +153,7 @@
 
         </div>
 
-        <div class="card-body" style="border-bottom: 1px solid black; background-color: white; font-weight: 500px; color: black;">
+        <div class="card-body" style="border-bottom: 1px solid black; background-color: white;">
             @if($penggunaAktif->count() > 0)
             <ul class="list-group mt-2">
                 @foreach($penggunaAktif as $pengguna)
