4f63ed43b502530bc5a60102e3626958a26afd98
Tamaris01 <tamarissilitonga@gmail.com>
2025-07-28 16:41:07 +0700

    Fix

--- resources/views/auth/landing-page.blade.php
+++ resources/views/auth/landing-page.blade.php
@@ -91,18 +91,19 @@
                 <span class="yellow">T</span><span class="black">erjamin</span>
             </span>
         </h2>
-        <section id="home" class="hero">
-            <p data-aos="fade-up" data-aos-delay="700">
-                Nikmati kemudahan parkir cerdas yang dirancang untuk <br>
-                keamanan dan kenyamanan Anda
-            </p>
 
-            <a href="#about" class="btn btn-yellow" data-aos="fade-up" data-aos-delay="900">
-                Selengkapnya <i class="fas fa-arrow-down"></i>
-            </a>
-        </section>
+        <p data-aos="fade-up" data-aos-delay="700">
+            Nikmati kemudahan parkir cerdas yang dirancang untuk <br>
+            keamanan dan kenyamanan Anda
+        </p>
 
-        <!-- PROBLEM STATEMENT SECTION -->
+        <a href="#about" class="btn btn-yellow" data-aos="fade-up" data-aos-delay="900">
+            Selengkapnya <i class="fas fa-arrow-down"></i>
+        </a>
+    </section>
+
+    <!-- PROBLEM STATEMENT SECTION -->
+    <section id="home" class="hero">
         <div class="row mt-5 text-center">
             <div class="col-md-12">
                 <h2 class="fw-bold mb-3">Masih punya area parkir yang belum aman dan terkelola dengan baik?</h2>
